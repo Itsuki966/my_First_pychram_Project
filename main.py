a7a7f0b5a1670c88c8c04bdc9a4d4c3222dab0bc
Itsuki966 <92083270+Itsuki966@users.noreply.github.com>
2021-12-21 17:52:18 +0900

    what is your name

--- main.py
+++ main.py
@@ -6,11 +6,12 @@
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+    print('Hi, %s' % name)  # Press Ctrl+F8 to toggle the breakpoint.
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    name = input("What is your name?")
+    print_hi('name')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
